f4db78b0392d7f6487a92bd1f28b47f73b406bca
Ersani <mr.ersani@gmail.com>
2025-09-25 11:44:21 +0700

    3
    ketiga

--- resources/views/formulir/create.blade.php
+++ resources/views/formulir/create.blade.php
@@ -160,8 +160,8 @@
                         <!-- Ijazah (Multiple) -->
                         <div>
                             <label for="ijazah" class="block text-sm font-medium text-gray-700 mb-2">
-                                Ijazah <span class="text-red-500">*</span>
-                                <span class="text-xs text-gray-500">( SD s/d Ijazah Terakhir)</span>
+                                Ijazah SD s/d Terakhir <span class="text-red-500">*</span>
+                                <span class="text-xs text-gray-500">(bisa upload beberapa file sekaligus)</span>
                             </label>
                             <input type="file" id="ijazah" name="ijazah[]" multiple
                                 class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700"
@@ -177,8 +177,8 @@
                         <!-- Transkrip Nilai (Multiple) -->
                         <div>
                             <label for="transkrip_nilai" class="block text-sm font-medium text-gray-700 mb-2">
-                                Transkrip Nilai <span class="text-red-500">*</span>
-                                <span class="text-xs text-gray-500">( SD s/d  Terakhir)</span>
+                                Transkrip Nilai SD s/d Teralhir<span class="text-red-500">*</span>
+                                <span class="text-xs text-gray-500">(bisa upload beberapa file sekaligus)</span>
                             </label>
                             <input type="file" id="transkrip_nilai" name="transkrip_nilai[]" multiple
                                 class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700"
