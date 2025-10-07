49c7e2045c7b568773a0de3204c5a765710c3e78
Ersani <mr.ersani@gmail.com>
2025-10-07 12:38:51 +0700

    edit
    edit multiple

--- app/Models/Formulir.php
+++ app/Models/Formulir.php
@@ -28,4 +28,24 @@ class Formulir extends Model
         'ijazah' => 'array',
         'transkrip_nilai' => 'array',
     ];
+
+    public function setIjazahAttribute($value)
+    {
+        if (is_string($value)) {
+            $decoded = json_decode($value, true);
+            $this->attributes['ijazah'] = json_encode($decoded ?: []);
+        } else {
+            $this->attributes['ijazah'] = json_encode($value ?: []);
+        }
+    }
+
+    public function setTranskripNilaiAttribute($value)
+    {
+        if (is_string($value)) {
+            $decoded = json_decode($value, true);
+            $this->attributes['transkrip_nilai'] = json_encode($decoded ?: []);
+        } else {
+            $this->attributes['transkrip_nilai'] = json_encode($value ?: []);
+        }
+    }
 }
